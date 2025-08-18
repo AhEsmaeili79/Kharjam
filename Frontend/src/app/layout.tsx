3b916aeefb3fb8e56dc742deaed4178ca043c01a
hedieh <heydari.hedieh2680@gmail.com>
2025-08-18 15:13:42 +0330

    chore: install and config tailwind and shadcn

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,11 +1,22 @@
 "use_client"
+import './globals.css'
+import { Button } from "@/components/ui/button"
+import Home from './home/page';
+
+
 export default function RootLayout() {
   return (
     <html lang="en">
-      <body>
-      <h1>
+      <body  className="bg-gray-500 size-full">
+      <h1 className="text-green-600">
         the layout
         </h1>
+
+        <div>
+      <Button variant="default">shadcn button</Button>
+    </div>
+    <Home/>
+        
       </body>
     </html>
   );
