154e76c79f64cb496306f28749043396a6ba3352
hedieh <heydari.hedieh2680@gmail.com>
2025-09-06 22:30:24 +0330

    style: init color pallete

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,7 +1,5 @@
-// "use_client"
+"use client"
 import "./globals.css";
-// import { Button } from "@/components/ui/button"
-// import Home from './home/page';
 
 import { Button } from "@/components/ui/button";
 import { ReactNode } from "react";
@@ -10,11 +8,10 @@ import Home from "./page";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-gray-500 size-full">
-        <h1 className="text-green-600">the layout</h1>
-        <main className="p-4">{children}</main>
-        <div></div>
-      </body>
+      <body className="bg-primary-100 size-full px-6">
+            <h1 className="w-full text-base-100">desktop layout</h1>
+            <main className="scrollbar">{children}</main>
+          </body>
     </html>
   );
 }
