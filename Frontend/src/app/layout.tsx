bbd289600d267832bd3ad6496c202235e751b572
hedieh <heydari.hedieh2680@gmail.com>
2025-09-18 21:29:08 +0330

    style:change style of chnage locale component

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -6,6 +6,7 @@ import { defaultLocale } from "@/i18n";
 import "./globals.css";
 import { useLocale } from "@/utils/useLocaleUtil";
 import ToggleThemeComponent from "@/components/ToggleThemeComponent";
+import ChangeLocaleComponent from "@/components/ChangeLocaleComponent";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const { currentLocale, messages } = useLocale();
@@ -17,6 +18,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <div className="w-full h-screen bg-[url('/bgLight.svg')] dark:bg-[url('/bgDark.png')] bg-no-repeat bg-cover bg-center flex items-center justify-center relative">
          <div className="absolute top-5 right-5">
           <ToggleThemeComponent/>
+         </div>
+         <div className="absolute top-5 left-5">
+            <ChangeLocaleComponent/>
          </div>
             {children}
           </div>
